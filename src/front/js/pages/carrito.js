d2ab58fd46ad098db36f4e7cf2f8821504b8cd05
GabRoma <gabrielrg901@gmail.com>
2023-02-03 16:31:18 +0000

    ajustes menores

--- src/front/js/pages/carrito.js
+++ src/front/js/pages/carrito.js
@@ -72,9 +72,11 @@ export const Carrito = (props) => {
             {/* <div className="card-header">Resumen del pedido</div> */}
             <div className="card-body">
               <p className="card-text">Productos:</p>
-              <p className="card-text">Descuentos:</p>
-              <h5 className="card-title">Total:</h5>
-              <a href="#" className="btn btn-dark">
+              <p className="card-text text-muted">Descuentos:</p>
+              <div className="mt-5 mb-5">
+                <h5 className="card-text">Total: $</h5>
+              </div>
+              <a href="#" className="btn btn-dark w-100">
                 Proceder al pago
               </a>
             </div>
