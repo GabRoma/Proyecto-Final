7e64283ada1cd3ed4352420ae8889ac0509d8022
agustinaf18 <agussslp1529@gmail.com>
2023-02-07 19:58:02 +0000

    login formulario linkeado al navbar, hay que linkear el registro a la alert del login

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,17 +5,47 @@ import { Login } from "../component/login";
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-light bg-light">
-      <Login />
-      <div className="container">
-        {/* <Link to="/">
-          <span className="navbar-brand mb-0 h1">React Boilerplate</span>
-        </Link> */}
-        <div className="ml-auto">
-          {/* <Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link> */}
+      <div
+        className="modal fade"
+        id="divpadre"
+        aria-hidden="true"
+        aria-labelledby="exampleModalToggleLabel"
+        tabindex="-1"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="">
+              <button
+                type="button"
+                className="btn-close float-end my-1 mx-1"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="">
+              <Login />
+            </div>
+            <div className="my-2 mx-1"></div>
+            <div class="">
+              {/* <button
+                class="btn btn-primary"
+                data-bs-target="#exampleModalToggle2"
+                data-bs-toggle="modal"
+              >
+                Open second modal
+              </button> */}
+            </div>
+          </div>
         </div>
       </div>
+      <a
+        class="btn btn-primary position-absolute top-0 end-0 my-2 mx-5 px-3"
+        data-bs-toggle="modal"
+        href="#divpadre"
+        role="button"
+      >
+        ingresar
+      </a>
     </nav>
   );
 };
