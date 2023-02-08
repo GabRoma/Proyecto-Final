2aa2300246c6d60894e63dc61af1a505cceaa394
agustinaf18 <agussslp1529@gmail.com>
2023-02-08 23:14:58 +0000

    casi listo

--- src/front/js/component/login.js
+++ src/front/js/component/login.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { Context } from "../store/appContext.js";
 import { Link } from "react-router-dom";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 export const Login = () => {
+  const { store, actions } = useContext(Context);
+
   return (
     <>
       <Formik
@@ -11,7 +14,7 @@ export const Login = () => {
           inputLogincorreo: "",
           inputLogincontrasena: "",
         }}
-        validationSchema={Yup.object({
+        validationSchema={Yup.object().shape({
           inputLogincorreo: Yup.string()
             .email("Dirección de correo electrónico inválido")
             .required("Requerido"),
@@ -19,8 +22,16 @@ export const Login = () => {
           inputLogincontrasena: Yup.string().required("Requerido"),
         })}
         onSubmit={(values, { setSubmitting }) => {
+          console.log(values);
+          console.log(values.inputLogincorreo);
+
+
+          actions.inicioLogin(
+            values.inputLogincorreo,
+            values.inputLogincontrasena
+          );
           setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
+            // alert(JSON.stringify(values, null, 2));
             setSubmitting(false);
           }, 400);
         }}
@@ -30,7 +41,10 @@ export const Login = () => {
             className="card mx-auto border-secondary border-secondary border-2  rounded-5"
             style={{ width: 430, height: 480 }}
           >
-            <div class="card-header container mx-auto" style={{ height: 130 }}>
+            <div
+              className="card-header container mx-auto"
+              style={{ height: 130 }}
+            >
               <div className=" fw-bold my-2 fs-3 mx-auto">Inicio de Sesión</div>
               <div
                 className="fs-6 my-2 fw-light mx-auto"
@@ -39,107 +53,101 @@ export const Login = () => {
                 Ingresa tu correo electrónico, y contraseña para iniciar sesión
               </div>
             </div>
-            <ul className="list-group list-group-flush" style={{ height: 260 }}>
-              <li className="list-group-item" style={{ height: 130 }}>
-                <form className="row row-cols-lg-auto g-3 align-items-center my-1" />
-                <div className="col-12">
-                  {/* <label
-                className="visually-hidden"
-                for="inlineFormInputGroupUsername"
+            <Form className="row row-cols-lg-auto g-3 align-items-center my-1">
+              <ul
+                className="list-group list-group-flush mx-3"
+                style={{ height: 200, width:420 }}
               >
-                Usernamee
-              </label> */}
+                <li className="list-group-item" style={{ height: 130 }}>
+                  <div className="col-12">
+                    <div
+                      className="text-start fs-6 fw-light"
+                      style={{ height: 20 }}
+                    >
+                      Dirección de correo
+                    </div>
+                    <div className="input-group my-1">
+                      <div className="input-group-text">
+                        <i className="fa fa-envelope"></i>
+                      </div>
+                      <Field
+                        type="email"
+                        className={
+                          "form-control rounded-5" +
+                          (errors.inputLogincorreo && touched.inputLogincorreo
+                            ? "border border-danger border-2 "
+                            : "border-secondary border-2")
+                        }
+                        name="inputLogincorreo"
+                        placeholder="email@address.com"
+                        // value={email}
+                        // onChange={(e) => setEmail(e.target.value)}
+                      />
+                      <span className="text-danger d-block mx-2 my-1 ">
+                        <ErrorMessage name="inputLogincorreo" />
+                      </span>
+                    </div>
+                  </div>
+                </li>
+                <li className="list-group-item" style={{ height: 110 }}>
+                  {/* <form className="row row-cols-lg-auto g-3 align-items-center my-1" /> */}
+                  <div className="col-12" />
                   <div
-                    className="text-start fs-6 fw-light"
+                    className="text-start fs-6 fw-light "
                     style={{ height: 20 }}
                   >
-                    Dirección de correo
+                    Contraseña
                   </div>
                   <div className="input-group my-1">
                     <div className="input-group-text">
-                      <i class="fa fa-envelope"></i>
+                      <i className="fa fa-lock"></i>
                     </div>
                     <Field
-                      type="email"
-                      // className="form-control"
+                      type="password"
+                      // className="form-control fw-bold "
                       className={
-                        "form-control rounded-5" +
-                        (errors.inputLogincorreo && touched.inputLogincorreo
-                          ? "border border-danger border-2 "
-                          : "border-secondary border-2")
+                        "form-control rounded-5 " +
+                        (errors.inputLogincontrasena &&
+                        touched.inputLogincontrasena
+                          ? "border border-danger border-2  "
+                          : " border-2")
                       }
-                      name="inputLogincorreo"
-                      placeholder="email@address.com"
+                      name="inputLogincontrasena"
+                      placeholder="••••••••••••••"
                     />
-                    <span className="text-danger d-block mx-2 my-1 ">
-                      <ErrorMessage name="inputLogincorreo" />
+                    <span className="text-danger mx-2 my-1">
+                      <ErrorMessage name="inputLogincontrasena" />
                     </span>
                   </div>
-                </div>
-              </li>
-              <li className="list-group-item" style={{ height: 110 }}>
-                <form className="row row-cols-lg-auto g-3 align-items-center my-1" />
-                <div className="col-12" />
-                {/* <label
-              className="visually-hidden"
-              for="inlineFormInputGroupUsername"
-            >
-              Username
-            </label> */}
-                <div
-                  className="text-start fs-6 fw-light "
-                  style={{ height: 20 }}
-                >
-                  Contraseña
-                </div>
-                <div className="input-group my-1">
-                  <div className="input-group-text">
-                    <i class="fa fa-lock"></i>
+                  <div className=" fw-light text-start text-decoration-underline">
+                    ¿Olvidaste tu contraseña?
                   </div>
-                  <Field
-                    type="password"
-                    // className="form-control fw-bold "
-                    className={
-                      "form-control rounded-5 " +
-                      (errors.inputLogincontrasena &&
-                      touched.inputLogincontrasena
-                        ? "border border-danger border-2  "
-                        : " border-2")
-                    }
-                    name="inputLogincontrasena"
-                    placeholder="••••••••••••••"
-                  />
-                  <span className="text-danger mx-2 my-1">
-                    <ErrorMessage name="inputLogincontrasena" />
-                  </span>
-                </div>
-                <div className=" fw-light text-start text-decoration-underline">
-                  ¿Olvidaste tu contraseña?
-                </div>
-              </li>
-              <li className="list-group-item" style={{ height: 60 }}>
-                <form className="row row-cols-lg-auto g-3 align-items-center my-1" />
-                <div className="col-12" />
-                <div className="container my-2">
-                  <button
-                    class="btn btn-dark text-center my-2"
-                    style={{ width: 380 }}
-                    type="button"
-                  >
-                    Inicio de Sesión
-                  </button>
-                  <button
-                    class="btn btn-outline-dark text-center"
-                    data-bs-target="#divpadre2"
-                    data-bs-toggle="modal"
-                    style={{ width: 380 }}
-                    type="button"
-                  >
-                    Crear cuenta
-                  </button>
-                </div>
-              </li>
-            </ul>
+                </li>
+                <li className="list-group-item" style={{ height: 60 }}>
+                  {/* <form className="row row-cols-lg-auto g-3 align-items-center my-1" /> */}
+                  <div className="col-12" />
+                  <div className="container my-2 mx-auto">
+                    <button
+                      // onClick={enviarDatos}
+                      className="btn btn-dark text-center my-2"
+                      style={{ width: 350 }}
+                      type="submit"
+                    >
+                      Inicio de Sesión
+                    </button>
+                    <button
+                      className="btn btn-outline-dark text-center"
+                      data-bs-target="#divpadre2"
+                      data-bs-toggle="modal"
+                      style={{ width: 350 }}
+                      type="submit"
+                    >
+                      Crear cuenta
+                    </button>
+                  </div>
+                </li>
+              </ul>
+            </Form>
           </div>
         )}
       </Formik>
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,54 +1,98 @@
-const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+const getState = ({
+    getStore,
+    getActions,
+    setStore
+}) => {
+    return {
+        store: {
+            estalogueado: false,
+            message: null,
+            demo: [{
+                    title: "FIRST",
+                    background: "white",
+                    initial: "white",
+                },
+                {
+                    title: "SECOND",
+                    background: "white",
+                    initial: "white",
+                },
+            ],
+        },
+        actions: {
+            // Use getActions to call a function within a fuction
+            exampleFunction: () => {
+                getActions().changeColor(0, "green");
+            },
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+            getMessage: async () => {
+                try {
+                    // fetching data from the backend
+                    const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
+                    const data = await resp.json();
+                    setStore({
+                        message: data.message,
+                    });
+                    // don't forget to return something, that is how the async resolves
+                    return data;
+                } catch (error) {
+                    console.log("Error loading message from backend", error);
+                }
+            },
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+            inicioLogin: (userEmail, userPassword) => {
+                console.log("funciona");
+                fetch(
+                        "https://3001-gabroma-proyectofinal-7vc1rq3vbbk.ws-us86.gitpod.io/api/login", {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                                // 'Content-Type': 'application/x-www-form-urlencoded',
+                            },
+                            body: JSON.stringify({"email":userEmail,"password":userPassword
+						   }), //lo que tenga el recipiente reproducelo // body data type must match "Content-Type" header
+                        }
+                    )
+                    .then((response) => {
+                        if (response.status === 200) {
+                            setStore({
+                                estalogueado: true,
+                            });
+                        }
+                        return response.json();
+                    })
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+                    // nombre de donde se guard ,  el valor access_token se guarda en token
+                    .then((data) => {
+                        console.log(data);
+
+                        if (data.msg === "Bad email or password") {
+                            alert("data.msg");
+                        }
+
+                        localStorage.setItem("token", data.access_token);
+                    }) // nos llega un objeto llamado data y tiene una propiedad access_token
+                    .catch((err) => console.log(err));
+            },
+
+            changeColor: (index, color) => {
+                //get the store
+                const store = getStore();
+
+                //we have to loop the entire demo array to look for the respective index
+                //and change its color
+                const demo = store.demo.map((elm, i) => {
+                    if (i === index) elm.background = color;
+                    return elm;
+                });
+
+                //reset the global store
+                setStore({
+                    demo: demo,
+                });
+            },
+        },
+    };
 };
 
 export default getState;
